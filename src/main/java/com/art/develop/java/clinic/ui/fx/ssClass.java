46e25a3bc9f9fb01c3bd1ce5e7f706b07d1e336a
DarkLight <bentusy@tut.by>
2014-10-22 16:41:07 +0400

    Art

--- src/main/java/com/art/develop/java/clinic/ui/fx/ssClass.java
+++ src/main/java/com/art/develop/java/clinic/ui/fx/ssClass.java
@@ -3,10 +3,9 @@ package com.art.develop.java.clinic.ui.fx;
 /**
  * Created by Artem on 1988,20,10.
  */
-public class TestClass {
+public class ssClass {
 
-    public String strAdd(String s1, String s2) {
-        return s1+s2;
+    public String strAdd(String s1, String s2) { return s1+s2;
     }
 
 
